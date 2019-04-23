1ab97a4c380d11832db4afef14c9e9e0e09cc8df
SeanGibbons42 <seangibbons42@gmail.com>
2019-04-22 22:28:04 -0400

    Debugged App and Diagram Added Dimension Lines

--- abbe/main.py
+++ abbe/main.py
@@ -1,14 +1,16 @@
-import tkinter as kk
+import tkinter as tk
+import views
+import models
 
 class Controller():
     def __init__(self, parent):
-        self.v = views.View()
+        self.v = views.View(parent)
         self.m = models.Model()
 
 
 
 if __name__ == "__main__":
     app = tk.Tk()
-    frame = AbbeApp(app)
-    frame.place(relx = 0, rely=0, relheight=1, relwidth=1)
+    main = Controller(app)
+    # frame.place(relx = 0, rely=0, relheight=1, relwidth=1)
     app.mainloop()
--- abbe/views.py
+++ abbe/views.py
@@ -3,7 +3,7 @@ import tkinter as tk
 class View():
     def __init__(self, parent):
         self.mainframe = tk.Frame(parent)
-        self.mainframe.place(relx = 0, rely = 0, relheight = 1, relheight = 1)
+        self.mainframe.place(relx = 0, rely = 0, relheight = 1, relwidth = 1)
 
         self.diagram = AbbeDiagram(self.mainframe)
         self.diagram.place(relx = 0, rely = 0, relheight = 0.4, relwidth = 1)
@@ -20,49 +20,54 @@ class View():
 
 class AbbeDiagram(tk.Canvas):
     def __init__(self, parent, width = 0, height = 0):
-        #size widget
+        tk.Canvas.__init__(self, parent, height = height, width = width)
         self.bind("<Configure>", self.on_resize)
 
-        tk.Canvas.__init__(self, parent, height = height, width = width)
+    def draw_diagram(self):
+        self.delete("all")
 
-
-    def draw_diagram(self, height, width):
+        #object, focal, and image plane locations
         obj_x = self.x_convert(0.1)
-        obj_start = self.y_convert(0.25)
-        obj_end = self.y_convert(0.75)
-        self.create_line(obj_x, obj_start, obj_x, obj_y)
-
-        lens_xstart = self.x_convert(0.50-0.05)
-        lens_xend = self.x_convert(0.50+0.05)
-        lens_ystart = self.y_convert(0.25)
-        lens_yend = self.y_convert(0.75)
-        self.create_oval(lens_xstart, lens_ystart, lens_xend, lens_yend)
-
         fcl_x = self.x_convert(0.7)
-        fcl_start = self.y_convert(0.25)
-        fcl_end = self.y_convert(0.75)
-        self.create_line(fcl_x, fcl_start, fcl_x, fcl_end, dash=20)
-
         img_x = self.x_convert(0.9)
-        img_start =  self.y_convert(0.25)
-        img_end = self.y_convert(0.75)
-        self.create_line(img_x, img_start, img_x, img_end)
 
+        #top and bottom bounds to the diagram
+        top_y = self.y_convert(0.25)
+        bottom_y = self.y_convert(0.75)
+
+        self.create_line(obj_x, top_y, obj_x, bottom_y, width=10)
+
+        lens_xstart = self.x_convert(0.50-0.025)
+        lens_xend = self.x_convert(0.50+0.025)
+        self.create_oval(lens_xstart, top_y, lens_xend, bottom_y, fill = "#0abee9")
+
+        self.create_line(fcl_x, top_y, fcl_x, bottom_y, dash=20, width=5)
+        self.create_line(img_x, top_y, img_x, bottom_y, width=10)
+
+        offset = self.x_convert(0.01)
+        y = self.y_convert(0.85)
+        dim_height = self.y_convert(0.1)
+        lens_x = (lens_xstart+lens_xend)//2
+        self.draw_dimensionline(obj_x, lens_x, y, dim_height)
+        self.draw_dimensionline(lens_x, fcl_x, y, dim_height)
+        self.draw_dimensionline(fcl_x, img_x, y, dim_height)
+
+    def draw_dimensionline(self, start, end, y, height):
+        mid_y = self.y_convert(0.5)
+        offset = self.x_convert(0.01)
+        self.create_line(start, y, end, y, arrow="both", width=5)
+        self.create_line(start, y-height//2, start, y+height//2, width=3)
+        self.create_line(end, y-height//2, end, y+height//2, width=3)
+        # self.create_line()
 
     def x_convert(self, relx):
-        return relx*self.width
+        return relx*self.winfo_width()
 
     def y_convert(self, rely):
-        return rely*self.height
+        return rely*self.winfo_height()
 
     def on_resize(self, event):
-        print("resize")
-        tk.update()
-        if width == 0:
-            width = self.winfo_width()
-        if height == 0:
-            height = self.winfo_height()
-        self.draw_diagram(self.height, self.width)
+        self.draw_diagram()
 
 
 class PlanePlot():
